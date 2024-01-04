b064c571755c29f6d622dbd7f2620a8d8135b506
beatrizcarolina <beatrizcarolinasr@gmail.com>
2024-01-04 13:35:03 -0300

    feat: import ebooks, users and auth modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { EbooksModule } from './ebooks/ebooks.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, EbooksModule, UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
