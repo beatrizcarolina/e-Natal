7fc96ad70e6d480d8876beca71a6ca74b1c25f1a
beatrizcarolina <beatrizcarolinasr@gmail.com>
2024-01-04 13:51:11 -0300

    feat: guard module to implement routes authorization

---
+++ src/guards/auth.guard.ts
@@ -0,0 +1,23 @@
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from "@nestjs/common";
+import { Observable } from "rxjs";
+import { AuthService } from "src/auth/auth.service";
+
+@Injectable()
+export class AuthGuard implements CanActivate{
+
+    constructor(private readonly authService: AuthService) { }
+
+
+    async canActivate(context: ExecutionContext) {
+        const request = context.switchToHttp().getRequest(); 
+        const { authorization } = request.headers;
+
+        try {
+            const data = this.authService.checkToken((authorization ?? "").split(" ")[1]);
+            return true;
+        } catch (error) {
+            throw new UnauthorizedException();
+        }
+    } 
+
+}
