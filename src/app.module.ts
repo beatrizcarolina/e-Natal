4b70a00c8f4f9c1347688fe899a9bf684bb49621
beatrizcarolina <beatrizcarolinasr@gmail.com>
2024-01-04 23:28:59 -0300

    feat: add schedule imports and providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,12 @@ import { PrismaModule } from './prisma/prisma.module';
 import { EbooksModule } from './ebooks/ebooks.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { ScheduleModule } from '@nestjs/schedule';
+import { EbookEmailScheduler } from './schedulers/ebook-email-scheduler';
 
 @Module({
-  imports: [PrismaModule, EbooksModule, UsersModule, AuthModule],
+  imports: [PrismaModule, EbooksModule, UsersModule, AuthModule, ScheduleModule.forRoot()],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService,EbookEmailScheduler],
 })
 export class AppModule {}
